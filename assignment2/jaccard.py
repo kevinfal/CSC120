1166040f97732084d04b852baa70d25f6e281450
Kevin <kevin.fal360@gmail.com>
2019-09-07 16:35:25 -0700

    documented and formatted

--- assignment2/jaccard.py
+++ assignment2/jaccard.py
@@ -1,12 +1,13 @@
-'''
+"""
     File: Jaccard.py
     Author: Kevin Falconett
     Purpose: Calculate Jaccard Index of two sets
 
-'''
+"""
+
 
 def jaccard(set1, set2):
-    '''
+    """
         Calculates the jaccard index of two sets
 
         Parameters:
@@ -15,29 +16,30 @@ def jaccard(set1, set2):
 
         Returns:
             (float): Jaccard index of the two sets
-    '''
+    """
 
     if len(set1) == 0 and len(set2) == 0:
-        #if both sets are empty
+        # if both sets are empty
         return 1.0
-        
+
     intersect = len(set1.intersection(set2))
-    #gets how many elements are same
+    # gets how many elements are same
 
     union = len(set1.union(set2))
-    #amouont of unique elements total
+    # amouont of unique elements total
 
-    return intersect/union
+    return intersect / union
+
 
 def main():
     set1 = input("Set 1: ")
     set2 = input("Set 2: ")
 
     result = jaccard(set1, set2)
-    #calculate jaccard Index
+    # calculate jaccard Index
 
     print(result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-    
--- assignment2/ngram_sets.py
+++ assignment2/ngram_sets.py
@@ -1,10 +1,12 @@
-'''
+"""
     File: ngram_sets.py
     Author: Kevin Falconett
-'''
+    Purpose: Construct a set of ngrams
+"""
+
 
 def ngram(arglist, startpos, length):
-    '''
+    """
         Returns n-gram from a list of strings
 
         Arguments:
@@ -14,44 +16,61 @@ def ngram(arglist, startpos, length):
 
         Returns:
             list of strings from index startpos of arglist to approriate length
-    '''
+
+        Preconditions:
+            n >= 0
+    """
     if startpos < 0:
-        #if starting index is negative, convert it to it's positive index
+        # if starting index is negative, convert it to it's positive index
         startpos += len(arglist)
 
-    if startpos + length <= len(arglist):   
-        return arglist[startpos: startpos + length]
+    if startpos + length <= len(arglist):
+        return arglist[startpos : startpos + length]
     else:
-        #cannot make an ngram with the indices
+        # cannot make an ngram with the indices
         return []
 
-def ngram_sets(string,n):
 
-    if string == '':
+def ngram_sets(string, n):
+    """
+        Creates a set of ngrams
+
+        Parameters:
+            string (str): string
+            n (int): length of ngrams
+        
+        Return:
+            set of ngrams
+        
+        Preconditions:
+            n >= 0
+    """
+
+    if string == "":
         return "set()"
 
-    string = string.split(' ')
-    ngrams = collect(string,n)
+    string = string.split(" ")
+    ngrams = collect(string, n)
     ngrams = convert(ngrams)
     ngrams = set(ngrams)
     return ngrams
 
+
 def collect(strings, n):
-    '''
+    """
         Parameters:
             strings (list[str]): list of strings
             n (int): length of desired ngrams
         Returns:
             (list) list of all possible ngrams of length n in strings
-    '''
-
+    """
     returned = []
 
-    for startpos in range(len(strings) * -1,len(strings)):
-        #start at negative index to get every possible ngram
+    for startpos in range(len(strings) * -1, len(strings)):
+        # start at negative index to get every possible ngram
 
-        added = ngram(strings,startpos,n)
-        #construct ngram
+        added = ngram(strings, startpos, n)
+        # construct ngram
 
         if len(added) == 0:
             continue
@@ -59,8 +78,10 @@ def collect(strings, n):
             continue
         returned.append(added)
     return returned
+
+
 def convert(ngrams):
-    '''
+    """
         Converts 2d list containing strings (ngrams) to a list containing tuples with the same strings (ngrams)
 
         Parameters:
@@ -68,12 +89,13 @@ def convert(ngrams):
 
         Return:
             list[tuple(string)]: list of tuples containing strings
-    '''
+    """
     returned = []
 
     for ngram in ngrams:
         returned.append(tuple(ngram))
     return returned
+
 
 def main():
     string = input("String: ")
@@ -82,5 +104,5 @@ def main():
     print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
