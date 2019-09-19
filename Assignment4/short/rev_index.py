eb716227c38ad953a835d1a9caccf24d0a0b5f0e
Kevin <kevin.fal360@gmail.com>
2019-09-18 20:09:16 -0700

    I honestly don't feel like working on this rn,

---
+++ Assignment4/short/rev_index.py
@@ -0,0 +1,14 @@
+'''
+    File: rev_index.py
+    Author: Kevin Falconett
+'''
+
+def build_rev_index(fileobj):
+    pass
+
+def main():
+    file = open("File: ")
+    build_rev_index(file)
+
+if __name__ == '__main__':
+    main()
