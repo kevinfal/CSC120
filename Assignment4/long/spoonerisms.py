42044db75c2125a34842930ba91cf6d21e563da2
Kevin <kevin.fal360@gmail.com>
2019-09-24 16:08:22 -0700

    Passed 1 test case, got strip working

--- Assignment4/long/spoonerisms.py
+++ Assignment4/long/spoonerisms.py
@@ -48,6 +48,7 @@ def get_phrases():
             read = read.strip(remove)
             read = read.upper()
             read = read.split()
+            read = strip_phrases(read)
 
             phrases.append(read)
         except:
@@ -60,10 +61,14 @@ def strip_phrases(phrase):
 
     '''
     remove = '?,!)(.-!@#$%^&*()}{"'
+    returned = []
     for i in range(len(phrase)):
         word = phrase[i]
         word = word.strip(remove)
-    return phrase
+        word = word.strip('-')
+        returned.append(word)
+        
+    return returned
 
 
 def spoonerisms(phrase,dictionary,proDictionary):
@@ -88,43 +93,45 @@ def spoonerisms(phrase,dictionary,proDictionary):
                         for phonIndex2 in range(len(phonemeslist2)):
 
                             #print(phonemeslist2[phonIndex2])
-                            newPhoneme = phonemeslist1.copy()
-                            newPhoneme[phonIndex1] = phonemeslist2[phonIndex2]
-                            newPhoneme = tuple(newPhoneme) # tuple because lists aren't hashable
+                            if phonemeslist2[phonIndex2] != phonemeslist1[phonIndex1]:
+                                newPhoneme = phonemeslist1.copy()
+                                newPhoneme[phonIndex1] = phonemeslist2[phonIndex2]
+                                newPhoneme = tuple(newPhoneme) # tuple because lists aren't hashable
 
-                            # same as first replace
-                            newPhoneme2 = phonemeslist2.copy()
-                            newPhoneme2[phonIndex2] = phonemeslist1[phonIndex1]
-                            newPhoneme2 = tuple(newPhoneme2)
+                                # same as first replace
+                                newPhoneme2 = phonemeslist2.copy()
+                                newPhoneme2[phonIndex2] = phonemeslist1[phonIndex1]
+                                newPhoneme2 = tuple(newPhoneme2)
 
                             # find words for these new phonemes
-                            #print("phoneme1 {} phoneme1 {}".format(newPhoneme,newPhoneme2))
                             
-                            if newPhoneme in proDictionary:
-                                newWords1 = proDictionary[newPhoneme]
-                            else:
-                                continue
-                            if newPhoneme2 in proDictionary:
-                                newWords2 = proDictionary[newPhoneme2]
-                            else:
-                                continue
-                            #print("word1 {} word2 {}".format(newWords1,newWords2))
+                                if newPhoneme in proDictionary:
+                                    newWords1 = proDictionary[newPhoneme]
+                                    #print("phoneme1 {} word {}".format(newPhoneme,newWords1))
 
-                            # reconstruct phrase with new words
-                            for word1Index in range(len(newWords1)):
-                                for word2Index in range(len(newWords2)):
-                                    newPhrase = []
-                                    for i in range(len(phrase)):
-                                        if i == wordIndex:
-                                            newPhrase.append(newWords1[word1Index])
-                                        elif i == wordIndex2:
-                                            newPhrase.append(newWords2[word2Index])
-                                        else:
-                                            newPhrase.append(phrase[i])
+                                else:
+                                    continue
+                                if newPhoneme2 in proDictionary:
+                                    newWords2 = proDictionary[newPhoneme2]
+                                else:
+                                    continue
+                                #print("word1 {} word2 {}".format(newWords1,newWords2))
 
-                                    # add the new phrase to returned
-                                    if newPhrase not in returned:
-                                        returned.append(newPhrase)
+                                # reconstruct phrase with new words
+                                for word1Index in range(len(newWords1)):
+                                    for word2Index in range(len(newWords2)):
+                                        newPhrase = []
+                                        for i in range(len(phrase)):
+                                            if i == wordIndex:
+                                                newPhrase.append(newWords1[word1Index])
+                                            elif i == wordIndex2:
+                                                newPhrase.append(newWords2[word2Index])
+                                            else:
+                                                newPhrase.append(phrase[i])
+
+                                        # add the new phrase to returned
+                                        if newPhrase not in returned:
+                                            returned.append(newPhrase)
                     
     return returned
 
@@ -155,39 +162,41 @@ def build_phonemeDict(dictionary):
 
 def printOutput(spoonerisms,phrase):
     for x in spoonerisms:
-        x = ' '.join(x)
         if x != phrase:
             print(x)
 
 def main():
-    #filename = 'PronunciationDictionary.txt'
     filename = input()
     phrases = get_phrases()
 
     for phrase in phrases:
         phraseJoined = " ".join(phrase)
         print('Phrase: {}'.format(phraseJoined))
+
         dictionary = build_dict(filename)
         dic2 = build_phonemeDict(dictionary)
         result = spoonerisms(phrase,dictionary,dic2)
 
         result = joinPhrases(result)
         result = sorted(result)
-        printOutput(result,phraseJoined)
+
+        printOutput(result, phraseJoined)
 
 def debug():
 
     #reading from input file (txt)
-    inputfile = '0.in'
+    inputfile = '1.in'
     #inputfile = input("input")
     inputs = open(inputfile)
     dictionaryFile = inputs.readline().strip('\n')
     phrases = []
 
+    
     for line in inputs:
-        line = line.strip('\n')
+        line = line.strip('?,!)(.-!@#$%^&*()}{"')
         line = line.upper()
         line = line.split()
+        line = strip_phrases(line)
         phrases.append(line)
 
     for phrase in phrases:
@@ -198,6 +207,7 @@ def debug():
         dict2 = build_phonemeDict(dictionary)
 
         result = spoonerisms(phrase,dictionary,dict2)
+        result = joinPhrases(result)
         result = sorted(result)
 
         printOutput(result,phraseJoined)
@@ -208,4 +218,4 @@ def debug():
 
 if __name__ == '__main__':
     main()
-    #debug()
+    debug()
