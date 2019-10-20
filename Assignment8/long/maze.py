75d307bb37ce9887f88096550e8dd9ee1c35a57e
Kevin <kevin.fal360@gmail.com>
2019-10-20 12:46:13 -0700

    added documentation and finished pretty much

--- Assignment8/long/maze.py
+++ Assignment8/long/maze.py
@@ -2,7 +2,9 @@
     File: maze.py
     Author: Kevin Falconett
     Purpose: Provides class Maze that represents a maze
-             
+             the way the maze is represented is a dictionary
+             with edges as keys and other edges it connects to as
+             values
 """
 
 class Maze:
@@ -10,19 +12,34 @@ class Maze:
         self._maze = self._build_maze(edge_list)
     
     def _build_maze(self, edge_list):
-        
+        """
+        builds a "maze", constructs a dictionary with
+        an edge as key and it's connecting edges as
+        values
+        :param edge_list: (List[][char]) list of lists with
+                          [edge, connecting edge]
+        :return: (dict) with edges as keys and their connecting
+                 in arrays as values
+        """
         returned = dict()
-
         for edges in edge_list:
             edge1 = edges[0]
             edge2 = edges[1]
-
+            # if edge1 is not already in edges, will create []
+            # and append, else it just appends
             returned.setdefault(edge1,[]).append(edge2)
         
         return returned
 
     def solve(self, source, finish):
-
+        """
+        solves the maze and returns the path
+        that it took to get from source to
+        finish
+        :param source: (char) place to navigate from
+        :param finish: (char) end destination to get to
+        :return: (List[char]) of edges visited to get to finish
+        """
         if source == finish:
             return finish
         else:
@@ -40,20 +57,33 @@ class Maze:
 
 
 def create_edge_list(filename):
-    
+    """
+    parses through a maze file and constructs
+    a list[][char] of edges
+    :param filename: (str) name of file
+    :return: list[][char] of edges
+    """
     returned = []
     file = open(filename)
     for line in file:
         line = line.split()
+        # after splitting, line will be [edge1, edge2]
         returned.append(line)
     return returned
 
 
 
 def main():
-    edges = create_edge_list("in09.txt")
-    maze = Maze(edges)
-    print(maze.solve('a','d'))
+    """
+    prompts user for file and solves the maze in the file
+    :return:
+    """
+    try:
+        edges = input("File: ")
+        maze = Maze(edges)
+        print(maze.solve('a','d'))
+    except:
+        print("invalid file")
 
 if __name__ == "__main__":
     main()
