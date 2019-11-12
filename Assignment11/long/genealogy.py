852d08dc68bfe13bd09c0d2d8eeb9b160e02f461
Kevin <kevin.fal360@gmail.com>
2019-11-11 22:18:47 -0700

    finished build_fam or whatever, need path now

--- Assignment11/long/genealogy.py
+++ Assignment11/long/genealogy.py
@@ -9,6 +9,7 @@ class BST:
         self.val = val
         self.left = None
         self.right = None
+
     def insert(self, added):
         added_node = BST(added)
         if self.val == added:
@@ -28,21 +29,74 @@ class BST:
                     self.right.insert(added)
             else:
                 return
+
+
+    def search(self, target):
+        
+        if self.val == target:
+            return self
+        else:
+            if target < self.val:
+                
+                if self.left is None:
+                    return None
+                else:
+                    return self.left.search(target)
+            elif target > self.val:
+                if self.right is None:
+                    return None
+                else:
+                    return self.right.search(target)
+            else:
+                return None
+    
+
+    def path_strings(self, name):
+        target = Person(name)
+        if self.val == target:
+            return target.name
+        else:
+            returned = self.val.name
+            returned = returned + 
+    
     def __str__(self):
         val = str(self.val)
         left = str(self.left.val) if self.left is not None else None
         right = str(self.right.val) if self.right is not None else None
 
-        return "Val: {} Left: {} Right: {}".format(val,self,right)
+        return "Val: {} Left: {} Right: {}".format(val,left,right)
 
 class Person:
     def __init__(self,name):
         self.name = name
+        self.parent = None
+        self.rel = []
+    def add_rel_name(self,name:str):
+        added = Person(name)
+        self.rel.append(added)
+        added.parent = self
+    def add_rel_person(self, added):
+        self.rel.append(added)
+        added.parent = self
+
+
+    def __eq__(self, other):
+        return self.name == other.name
+    def __lt__(self, other):
+        return self.name < other.name
+    def __le__(self, other):
+        return self.name <= other.name
+    def __gt__(self,other):
+        return self.name > other.name
+    def __ge__(self,other):
+        return self.name >= other.name
+
+    def __str__(self):
+        return "Name: {}".format(self.name)
 
 
 
-
-def build_family_tree(rels):
+def build_family_tree(rels: list):
     """
         Parameters:
             rels List(Tuple): list of parent-child relationships
@@ -54,10 +108,46 @@ def build_family_tree(rels):
                 - family_tree is a pointer to the root of the family tree corresponding
                   to the relationships given 
     """
-    pass
+    lookup = create_lookup(rels)
+    lookup = update_relationships(lookup,rels)
+    fam = lookup.val
+
+    while fam.parent is not None:
+        fam = fam.parent
     
+    return lookup,fam
 
         
+        
+
+def update_relationships(lookup,rels):
+    for relation in rels:
+        name1 = relation[0]
+        name2 = relation[1]
+        search_person1 = Person(name1)
+        search_person2 = Person(name2)
+        
+        person1 = lookup.search(search_person1).val
+        person2 = lookup.search(search_person2).val
+        
+        person1.add_rel_person(person2)
+    return lookup
+
+
+def create_lookup(rels):
+    lookup = None
+    for relation in rels:
+        name1 = relation[0]
+        name2 = relation[1]
+        person1 = Person(name1)
+        person2 = Person(name2)
+        if not lookup:
+            lookup = BST(person1)
+            lookup.insert(person2)
+        else:
+            lookup.insert(person1)
+            lookup.insert(person2)
+    return lookup
 
 def get_relationship(family_tree,name1,name2):
     """
@@ -76,11 +166,20 @@ def get_relationship(family_tree,name1,name2):
     pass
 
 def main():
-    x = BST(5)
-    x.insert(2)
-    x.insert(1)
-    print(x)
-    print(x.left)
+    rels = [ ("Susana_Shakespeare", "Elizabeth_Hall"),  ("William_Shakespeare", "Hamnet_Shakespeare"),
+            ("William_Shakespeare", "Judith_Shakespeare"),  ("William_Shakespeare", "Susana_Shakespeare") ]
+
+    rels2 = [("A","B"),("B","C")]
+    tree,fam = build_family_tree(rels2)
+
+    print(fam)
+    for x in fam.rel:
+        print(x)
+        for y in x.rel:
+            print(y)
+
+    
+        
 
 
 if __name__ == "__main__":
